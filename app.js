1508f192649404a7943dc9bac74e14c357500a02
Digambar <deshawaldigambar@gmail.com>
2021-09-22 11:24:45 +0530

    converted date to string
    added function to convert date from number to sting

--- app.js
+++ app.js
@@ -11,10 +11,39 @@ function checkPalindrome(str){
     }
     else{
         return "Not palindrome";
-    }
-    
+    }    
 
 }
-console.log(checkPalindrome("hello"));
-console.log(checkPalindrome("dad"));
+
+
+function convertDateToStr(date){
+
+    var dateStr={day:'', month:'', year:''};
+
+    if(date.day < 10){
+        dateStr.day='0'+ date.day;
+    }
+    else{
+        dateStr.day= date.day.toString();
+    }
+    if(date.month<10){
+        dateStr.month='0'+date.month;
+    }
+    else{
+        dateStr.month= date.month.toString();
+    }
+    dateStr.year=date.year.toString();
+    return dateStr;
+}
+
+
+var date1  = {
+    day:14,
+    month:9,
+    year:2020,
+};
+
+
+console.log(convertDateToStr(date1));
+
 
