d6793e71550a411aad563ceed405ebd25ec04770
Digambar <deshawaldigambar@gmail.com>
2021-09-22 09:45:37 +0530

    reverseSting function

---
+++ app.js
@@ -0,0 +1,11 @@
+function reverseString(str){
+    var reverseStr=str.split("").reverse().join("");
+    return reverseStr
+
+
+}
+
+console.log(reverseString("hello"));
+console.log(reverseString("10012"));
+console.log(reverseString("mynameis"));
+console.log(reverseString("dad"));
